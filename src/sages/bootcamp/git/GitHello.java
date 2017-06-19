3e9306d154c1fb4c0e8db30b910b1d70dece27b5
Pawel Krowicki <pkrowicki@outlook.com>
2017-06-19 15:48:18 +0200

    Revert "Zadanie 2 commit"
    This reverts commit b918ea720b87108bb597be82a10521b7f965877f.
    
    # Conflicts:
    #	src/sages/bootcamp/git/GitHello.java

--- src/sages/bootcamp/git/GitHello.java
+++ src/sages/bootcamp/git/GitHello.java
@@ -4,8 +4,8 @@ package sages.bootcamp.git;
  * Created by Pawel Krowicki on 2017-06-19.
  */
 public class GitHello {
-        System.out.println("GIT HIT");
     public static void main(String[] args) {
+        System.out.println("GIT HIT");
         System.out.println("Hello GIT");
         System.out.println("HELLO GIT 2");
         System.out.println("HELLO GIT 3");
