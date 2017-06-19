08745b9c0d1cb2f4732913d08fe07b942a4dde72
Pawel Krowicki <pkrowicki@outlook.com>
2017-06-19 13:56:55 +0200

    Hello Git

---
+++ src/sages/bootcamp/git/GitHello.java
@@ -0,0 +1,10 @@
+package sages.bootcamp.git;
+
+/**
+ * Created by Pawel Krowicki on 2017-06-19.
+ */
+public class GitHello {
+    public void gitHello(){
+        System.out.println("Hello GIT!");
+    }
+}
